6d35a26d10643d1f5f654a8064f1cf2eaedcb402
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-11-27 18:24:22 +0200

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,38 @@
+const langArr = {
+    "unit":{
+        "en": "b22332 ",
+        "uk": "створює23232сайт",
+    }, 
+    "chip": {
+        "en": "graphics processing unit",
+        "uk": "графічний чіп",
+    }, 
+    "memory": {
+        "en": "memory size",
+        "ua": "oбсяг пам'яті",
+    }, 
+    "memory-type": {
+        "en": "memory type",
+        "uk": "nип пам'яті",
+    }, 
+    "cool": {
+        "en": "сooling system type",
+        "uk": "тип системи охолодження",
+    }, 
+    "more": {
+        "en": "more details",
+        "uk": "детально про товар",
+    }, 
+    "vk1":{
+        "en": "Hello1",
+        "uk": "Привіт1", 
+    },
+    "pr":{
+        "en": "Hello2",
+        "uk": "Привіт2", 
+    },
+    "fb":{
+        "en": "Hello3",
+        "uk": "Привіт3", 
+    },
+}
