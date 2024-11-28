a74071eab10433a96481b2f07f371077303a489e
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-11-28 11:46:00 +0200

    Update lang.js

--- lang.js
+++ lang.js
@@ -3,25 +3,9 @@ const langArr = {
         "en": "Furiya AI - EN",
         "uk": "Furiya AI - UA",
     }, 
-    "chip": {
-        "en": "graphics processing unit",
-        "uk": "графічний чіп",
-    }, 
-    "memory": {
-        "en": "memory size",
-        "ua": "oбсяг пам'яті",
-    }, 
-    "memory-type": {
-        "en": "memory type",
-        "uk": "nип пам'яті",
-    }, 
-    "cool": {
-        "en": "сooling system type",
-        "uk": "тип системи охолодження",
-    }, 
-    "more": {
-        "en": "more details",
-        "uk": "детально про товар",
+    "mov": {
+        "en": "Language",
+        "uk": "Мова",
     }, 
     "vk1":{
         "en": "Hello1",
