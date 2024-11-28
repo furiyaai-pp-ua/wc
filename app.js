e242d896627409b3f4c0a5670febe9b863d27c75
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-11-28 15:19:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const maxImg = document.querySelector('.right-panel img');
 const select = document.querySelector('select');
 const allLang = ['en', 'uk', ];
 
+const langArr = {
+    "unit" :  {
+        "en": "build a multi language site ",
+        "ua": "створюємо багатомовний сайт",
+    }, 
+    "chip": {
+        "en": "graphics processing unit",
+        "ua": "графічний чіп",
+    }, 
+    "memory": {
+        "en": "memory size",
+        "ua": "oбсяг пам'яті",
+    }, 
+    "memory-type": {
+        "en": "memory type",
+        "ua": "nип пам'яті",
+    }, 
+    "cool": {
+        "en": "сooling system type",
+        "ua": "тип системи охолодження",
+    }, 
+    "more": {
+        "en": "more details",
+        "ua": "детально про товар",
+    }, 
+}
+
+
 document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 
 select.addEventListener('change', changeURLLanguage);
@@ -33,36 +61,4 @@ function changeLanguage() {
     }
 }
 
-const langArr = {
-    "unit" :  {
-        "ru": "создаем многоязычный сайт",
-        "en": "build a multi language site ",
-        "ua": "створюємо багатомовний сайт",
-    }, 
-    "chip": {
-        "ru": "графический чип",
-        "en": "graphics processing unit",
-        "ua": "графічний чіп",
-    }, 
-    "memory": {
-        "ru": "объем памяти",
-        "en": "memory size",
-        "ua": "oбсяг пам'яті",
-    }, 
-    "memory-type": {
-        "ru": "тип памяти",
-        "en": "memory type",
-        "ua": "nип пам'яті",
-    }, 
-    "cool": {
-        "ru": "тип системы охлаждения",
-        "en": "сooling system type",
-        "ua": "тип системи охолодження",
-    }, 
-    "more": {
-        "ru": "детально о товаре",
-        "en": "more details",
-        "ua": "детально про товар",
-    }, 
-}
 changeLanguage();
