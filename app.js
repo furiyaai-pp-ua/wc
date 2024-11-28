a786b044e45a0ae30f6161552858a54602ba4ac7
Anmenig <35890296+anmenig@users.noreply.github.com>
2024-11-28 12:29:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const maxImg = document.querySelector('.right-panel img');
 const select = document.querySelector('select');
-const allLang = ['uk', 'en', ];
+const allLang = ['en', 'uk', ];
 
 document.querySelectorAll('.left-panel img').forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 
